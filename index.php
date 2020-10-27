4f3b11b7a559aa771c8477772124071a0217e674
sskcomjp <67447926+sskcomjp@users.noreply.github.com>
2020-10-27 10:13:42 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
-$comment = $_GET[‘comment’];
-echo $comment;
+$text = $_GET["val"]; //valの値を受け取り変数に格納
+//val.txtに値を上書き保存
+$A = file_put_contents('/var/www/html/samplePhp/val.txt',$text,FILE_APPEND);
+//val.txtの値を読み込み変数$sに格納
+$s = file_get_contents('/var/www/html/samplePhp/val.txt');
+print($s);
 ?>
